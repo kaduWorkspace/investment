8ad8ae33b0838d7c2dc547a23a321d205ea6e5e6
kaduhod <carlosjr.ribas@gmail.com.br>
2025-05-22 22:10:55 -0300

    :bug: Fix: Removed wrong tags

--- core/domain/http/dto.go
+++ core/domain/http/dto.go
@@ -3,31 +3,36 @@ package http
 import "time"
 
 type InvestmentInput struct {
-    Periods int
-    TaxDecimal float64
+    Periods int `json:"periods" form:"periods"`
+    TaxDecimal float64 `json:"tax_decimal" form:"tax_decimal"`
 }
+
 type SeriesInvestmentInput struct {
-    FirstDay bool
-    Contribution float64
+    FirstDay bool `json:"first_day" form:"first_day"`
+    Contribution float64 `json:"contribution" form:"contribution"`
 }
+
 type CoumpoundInterestInput struct {
     InvestmentInput
-    InitialValue float64
+    InitialValue float64 `json:"initial_value" form:"initial_value"`
 }
+
 type FutureValueOfASeriesInput struct {
     InvestmentInput
     SeriesInvestmentInput
 }
+
 type FutureValueOfASeriesWithPeriodsInput struct {
     InvestmentInput
     SeriesInvestmentInput
-    InitialValue float64
-    InitialDate time.Time
+    InitialValue float64 `json:"initial_value" form:"initial_value"`
+    InitialDate time.Time `json:"initial_date" form:"initial_date"`
 }
-type PredictContributionFVSInputs struct {
+
+type PredictContributionFVSInput struct {
     InvestmentInput
-    FinalValue float64
-    InitialValue float64
-    ContributionOnFirstDay bool
+    FinalValue float64 `json:"final_value" form:"final_value"`
+    InitialValue float64 `json:"initial_value" form:"initial_value"`
+    ContributionOnFirstDay bool `json:"contribution_on_first_day" form:"contribution_on_first_day"`
 }
 
