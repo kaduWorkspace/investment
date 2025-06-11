1e935ef5d92b99967f1b8afb6c332e796d061efe
kaduhod <carlosjr.ribas@gmail.com.br>
2025-06-10 21:27:45 -0300

    :construction: Feat: Added create user service

---
+++ core/application/service/createUserService.go
@@ -0,0 +1,42 @@
+package service
+
+import (
+	"errors"
+	"fmt"
+	app_dto "kaduhod/fin_v3/core/application/dto"
+	"kaduhod/fin_v3/core/domain/repository"
+	"kaduhod/fin_v3/core/domain/user"
+
+	"golang.org/x/crypto/bcrypt"
+)
+type CreateUserService struct {
+    repository repository.Repository[user.User]
+}
+func NewCreateUserService(repository repository.Repository[user.User]) user.CreateUserServiceI[app_dto.CreateUserInput] {
+    return &CreateUserService{
+        repository: repository,
+    }
+}
+func (s *CreateUserService) Create(usr app_dto.CreateUserInput) error {
+    hash , err := bcrypt.GenerateFromPassword([]byte(usr.Password), bcrypt.DefaultCost)
+    if err != nil {
+        fmt.Println(err)
+        return err
+    }
+    dbUser, err := s.repository.Get(user.User{Email: usr.Email})
+    if err != nil && err.Error() != "failed to get user: no rows in result set" {
+        return err
+    }
+    if dbUser.Email != "" {
+        fmt.Println(err)
+        return errors.New("User email not available")
+    }
+    if _, err := s.repository.Save(user.User{
+        Name:     usr.Name,
+        Password: string(hash),
+        Email:    usr.Email,
+    }); err != nil {
+        return err
+    }
+    return nil
+}
