93fbc93236d1bc8b2b0ae9be57b58665d666238f
kaduhod <carlosjr.ribas@gmail.com.br>
2025-06-20 21:31:54 -0300

    :hammer: Fix: Error running tests in dev

--- scripts/copy_env.sh
+++ scripts/copy_env.sh
@@ -2,5 +2,5 @@
 set -e  # Exit immediately if any command fails
 scp .env.development deployer@172.18.0.2:~/investment
 ssh deployer@172.18.0.2 <<EOF
-cat /home/deployer/.env /home/deployer/investiment/.env
+cp /home/deployer/.env /home/deployer/investiment/.env
 EOF
