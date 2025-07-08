22ad262fd7a3b10e12c26383da9b95a1a5d3a825
kaduhod <carlosjr.ribas@gmail.com.br>
2025-07-07 21:00:36 -0300

    :hammer: Feat: New dump generator and new data for the dump

--- core/infra/persistence/postgres/setup/getNewDump.sh
+++ core/infra/persistence/postgres/setup/getNewDump.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 docker exec fin-dev bash -c 'pg_dump -U postgres -Fc dev > /tmp/dev.dump'
-docker cp fin-dev:/tmp/dev.dump ./dump.sql
+docker cp fin-dev:/tmp/dev.dump ../migrations/dump.sql
 docker exec fin-dev rm /tmp/dev.dump
-echo "Dump created at ./dump.sql"
+DATA=$(date +'%d-%b-%Y' | tr '[:upper:]' '[:lower:]')  # exemplo: 07-jul-2025
+cp ../migrations/dump.sql ../migrations/dump.$DATA.sql
+echo "Dump criado: ../migrations/dump.sql e ../migrations/dump.$DATA.sql"
+
